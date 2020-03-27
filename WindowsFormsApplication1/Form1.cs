5c9eb0d52ba62a11b100842bcf037c4875bd1eae
ggLee <spahrose@naver.com>
2020-03-27 18:28:38 +0900

    초기 커밋.

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap bit = Properties.Resources.gg;
+
+            e.Graphics.DrawImage(bit, 0, 0);
+
+        }
+
+    }
+}
